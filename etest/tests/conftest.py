d839815de1b95f765ff8c59cd3f269c7900a8be1
MisterPot <english.man.spy@gmail.com>
2022-02-17 22:24:09 +0200

    Conn with db fixed

--- etest/tests/conftest.py
+++ etest/tests/conftest.py
@@ -6,7 +6,7 @@ from eshopadmin.db import db_sql_get_recordset_ex
 with open(etest.args_file) as file:
     raw_ids = file.read().split('\n')
     query = db_sql_get_recordset_ex('SELECT "ExternalShopId", "ScriptPath" FROM prod."ExternalShopsParsers" '
-                                    'WHERE "ExternalShopId" IN ({})'.format(', '.join(raw_ids)))
+                                    'WHERE "ExternalShopId" IN ({})'.format(', '.join(raw_ids)), ())
     ids = {item["ExternalShopId"]: item["ScriptPath"] for item in query}
 
 
