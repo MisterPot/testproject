8942c27d157b3ef30bcd2256d19c9d253933cc21
MisterPot <english.man.spy@gmail.com>
2022-02-17 22:14:50 +0200

    Try any variable

--- etest/__init__.py
+++ etest/__init__.py
@@ -1,6 +1,6 @@
 from os.path import join, normpath
 
 root = __path__[0]
-parser_file = normpath('/tests/test_parsers.py')
+parser_file = 'tests/test_parsers.py'
 args_file = join(root, 'data.txt')
 parser_test = join(root, parser_file)
